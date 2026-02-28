2783282ef6b304e0f4fe520783ddf7299b3bb545
severinn <severinnhq@gmail.com>
2026-02-28 20:22:31 +0100

    rename jokes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 
-import MirathelleAdvert from "@/components/MirathelleAdvert";
+import MirathelleAdvert from "@/components/MirathelleArticle";
 
 export default function Page() {
   return <MirathelleAdvert />;
